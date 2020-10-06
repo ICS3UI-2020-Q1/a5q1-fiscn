5c9fce059a25d8aa3a9d5486568b559925e9d31e
fiscn <fiscn2709@wrdsb.ca>
2020-10-06 13:08:11 +0000

    finished A5 Q1 program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 /**
- *
- * @author 
+ * Create multiplication tables from the user's number from 1-12 
+ * @author Neil Fischer
  */
 public class Main {
 
@@ -9,7 +11,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+    //prompt user to enter in a integer 
+    System.out.println("Please enter an integer to create a multiplication table for"); 
+
+    //declare a variable for user's number 
+    int userNM = input.nextInt(); 
+    //declare variable for multiplication answer 
+    int multi; 
+
+    //create for loop for the multiplication table 
+    for(int count1 = 1; count1 <= 12; count1++){
+    // loop action 
+    multi = count1 * userNM;
+    System.out.println(count1 + "X" + userNM + "=" + multi);
+    }
+
     
   }
 }
